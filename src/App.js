3835715b5e28a61e4d5f6f1ea7c6e91d6e35aa06
roche.oceane333-blip <roche.oceane333@gmail.com>
2026-01-15 12:33:11 +0100

    Correction imports et installation icônes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
+// Correction du chemin : on ajoute /Components/
 import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="flex h-screen w-full bg-[#030303] text-white overflow-hidden">
-      {/* Barre latérale à gauche */}
       <Sidebar />
-
-      {/* Zone de contenu principale à droite */}
       <main className="flex-1 flex flex-col items-center justify-center relative">
-        {/* Effet de lumière violette en fond */}
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-violet-600/10 rounded-full blur-[120px] pointer-events-none"></div>
-
         <div className="relative z-10 text-center">
           <h1 className="text-7xl font-black tracking-tighter italic mb-2">
             LIFE <span className="text-violet-500">OS</span>
